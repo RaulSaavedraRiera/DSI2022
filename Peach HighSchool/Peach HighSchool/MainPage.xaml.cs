122edfd3b33f80829c366b1298500298619e781d
AntonioPove <79278677+AntonioPove@users.noreply.github.com>
2022-04-01 01:23:00 +0200

    MENU MISIONES

--- Peach HighSchool/Peach HighSchool/MainPage.xaml.cs
+++ Peach HighSchool/Peach HighSchool/MainPage.xaml.cs
@@ -34,7 +34,7 @@ namespace Peach_HighSchool
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            Frame.Navigate(typeof(HudPrincipal));
+            Frame.Navigate(typeof(Misiones));
         }
 
         private void Button_Click_2(object sender, RoutedEventArgs e)
